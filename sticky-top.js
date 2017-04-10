5626f8ae9f65d1537f0ada531cc7729332d4d7b9
Pipit Shopia <shopiapipit@gmail.com>
2017-04-10 18:36:05 +0700

    Update sticky-top.js

--- sticky-top.js
+++ sticky-top.js
@@ -1,3 +1,4 @@
 $(function(){if($("#HTML1").length){var o=$("#HTML1"),t=$("#HTML1").offset().top,i=$("#HTML1").height();$(window).scroll(function(){var s=$("#banner728-bottom").offset().top-i-20,n=$(window).scrollTop(),f=$(window).width();if(n>t&&f>759?o.css({position:"fixed",top:50}):o.css("position","static"),n>s){var c=s-n;o.css({top:c})}})}});
 
-function scrollToTop(){verticalOffset="undefined"!=typeof verticalOffset?verticalOffset:0,element=$("body"),offset=element.offset(),offsetTop=offset.top,$("html, body").animate({scrollTop:offsetTop},600,"linear")}$(function(){$(document).on("scroll",function(){$(window).scrollTop()>100?$(".smoothscroll-top").addClass("show"):$(".smoothscroll-top").removeClass("show")}),$(".smoothscroll-top").on("click",scrollToTop)});
+//Back Top
+if($(".smoothscroll-top").length){var scrollTrigger=100,backToTop=function(){var o=$(window).scrollTop();o>scrollTrigger?$(".smoothscroll-top").addClass("show"):$(".smoothscroll-top").removeClass("show")};backToTop(),$(window).on("scroll",function(){backToTop()}),$(".smoothscroll-top").on("click",function(o){o.preventDefault(),$("html,body").animate({scrollTop:0},700)})};
